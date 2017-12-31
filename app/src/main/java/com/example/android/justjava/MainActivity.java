d27d86d9031544398aae46ae637650553164fcbe
pdjambazoff <djambazoff.github@gmail.com>
2017-12-31 15:29:24 -0800

    second push

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,95 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+package com.example.android.justjava;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int quantity = 2;
+    String message = "You total due is: $";
+    int pricePerCup = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int price = calculatePrice();
+        String name = "Phil Djambazoff";
+        displayMessage(createOrderSummary(price, name, quantity));
+    }
+
+
+    /**
+     * @param summary calculated price
+     * @return
+     */
+    private void displayMessage(String summary) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(summary);
+    }
+
+    /**
+     * @param priceNumber    total price
+     * @param name           of the client
+     * @param quantityNumber of ordered cups
+     * @return summary of order
+     */
+
+    private String createOrderSummary(int priceNumber, String name, int quantityNumber) {
+        String textSummary = "Name: " + name + "\nQuantity: " + quantityNumber
+                + "\nTotal Price: $" + priceNumber + "\nThank you!";
+        return textSummary;
+    }
+
+    /**
+     * @return total price
+     */
+    private int calculatePrice() {
+        return quantity * pricePerCup;
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * @param view xml view id
+     */
+    public void increase(View view) {
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * @param view xml view id
+     */
+    public void decrease(View view) {
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+}
